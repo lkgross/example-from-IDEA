6d5acfcd5736e82d8ceda36d8dcfb8e640d511be
lkgross <profgross@gmail.com>
2020-02-04 20:39:10 -0500

    First commit

---
+++ src/com/company/Printing.java
@@ -0,0 +1,26 @@
+/*
+ * Main.java
+ *
+ * A class to get started programming in Java
+ *
+ * Starter code by Laura K. Gross, COMP 152, Bridgewater State University
+ *
+ * Completed by: [student name], [student email]
+ *         date: [date of completion]
+ *
+ */
+
+package com.company;
+
+public class Printing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println();
+        System.out.println(99);
+        System.out.println(true);
+        System.out.println("A");
+        System.out.println('A');
+        System.out.println("ABC");
+    }
+}
